f4a5123c50ae35aa0bf963236f76946d30051d83
Yoshiki Hirata <kirryx2@gmail.com>
2021-03-04 14:52:36 +0900

    Delete db from git because it is too large

--- backend/apps/report/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Application: Report
-Description: Make report.
-  - 1. Start report project. Enter title
-  - 2. Choose premade header if you need.
-  - 3. Select team id or create team
-  - 4. update contents
-  - 5. Turn in report then give a timestamp
-"""
--- backend/src/boot.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../
-uvicorn main:app --host 0.0.0.0 --port 8080 --reload
